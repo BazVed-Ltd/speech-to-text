9ae11e69a22c09b54a7db372cde27f482920a69b
platofff <42435266+platofff@users.noreply.github.com>
2025-08-02 17:37:41 +0300

    video notes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM rocm/dev-ubuntu-24.04:6.2
+FROM rocm/dev-ubuntu-24.04:6.3
 RUN apt-get update && apt-get install -y python3-pip python3-dev
-RUN pip3 install --break-system-packages --no-cache-dir torch torchaudio --index-url https://download.pytorch.org/whl/rocm6.2
-RUN apt-get update && apt-get install -y ffmpeg
+RUN pip3 install --break-system-packages --no-cache-dir torch torchaudio --index-url https://download.pytorch.org/whl/rocm6.3
+RUN apt-get install -y ffmpeg
 
 WORKDIR /app
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import io
 import logging
 import asyncio
+import time
 import traceback
 
 from aiogram import Bot, Dispatcher, types
@@ -12,10 +13,10 @@ from transformers import pipeline, WhisperForConditionalGeneration, WhisperProce
 import soundfile as sf
 
 # Чтение переменных окружения
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ALLOWED_CHAT_ID = [int(x) for x in os.getenv('ALLOWED_CHAT_ID').split(',')]
+BOT_TOKEN = os.environ['BOT_TOKEN']
+ALLOWED_CHAT_ID = [int(x) for x in os.environ['ALLOWED_CHAT_ID'].split(',')]
 PYTORCH_DEVICE = os.getenv('PYTORCH_DEVICE')
-SPEECH_RECOGNITION_MODEL = os.getenv('SPEECH_RECOGNITION_MODEL')
+SPEECH_RECOGNITION_MODEL = os.environ['SPEECH_RECOGNITION_MODEL']
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -34,19 +35,17 @@ processor = WhisperProcessor.from_pretrained(SPEECH_RECOGNITION_MODEL)
 model = WhisperForConditionalGeneration.from_pretrained(SPEECH_RECOGNITION_MODEL)
 
 # Установка языка и задачи для принудительного декодирования (только русский)
-language = "ru"
-task = "transcribe"
-forced_decoder_ids = processor.get_decoder_prompt_ids(language=language, task=task)
-model.generation_config.forced_decoder_ids = forced_decoder_ids
+model.generation_config.language = 'ru'
+model.generation_config.task = 'transcribe'
 
 pipe = pipeline(
     "automatic-speech-recognition",
     model=model,
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
-    chunk_length_s=30,  # Позволяет обрабатывать аудио длиннее 30 секунд
     device=PYTORCH_DEVICE,
-    torch_dtype=torch_dtype
+    torch_dtype=torch_dtype,
+    return_timestamps=True
 )
 
 # Установка pad_token_id
@@ -55,7 +54,7 @@ if pipe.tokenizer.pad_token_id is None or pipe.tokenizer.pad_token_id == pipe.to
 logger.info("Модель распознавания речи успешно настроена.")
 
 @dp.message(Command("start"))
-async def start_message(message: types.Message):
+async def start_message(message: types.Message) -> None:
     if message.chat.id not in ALLOWED_CHAT_ID:
         await message.reply(f"Этот бот недоступен в этом чате. ID чата {message.chat.id}")
         return
@@ -65,8 +64,8 @@ async def start_message(message: types.Message):
         parse_mode='Markdown'
     )
 
-@dp.message(lambda message: message.voice)
-async def media_handler(message: types.Message):
+@dp.message(lambda message: message.voice or message.video_note)
+async def media_handler(message: types.Message) -> None:
     if message.chat.id not in ALLOWED_CHAT_ID:
         await message.reply(f"Этот бот недоступен в этом чате. ID чата {message.chat.id}")
         return
@@ -74,55 +73,62 @@ async def media_handler(message: types.Message):
     try:
         if message.voice:
             file_id = message.voice.file_id
-            file_size = message.voice.file_size
+            tmp_path = os.path.join('/app/tmp', f'{time.time_ns()}.ogg')
+        elif message.video_note:
+            file_id = message.video_note.file_id
+            tmp_path = os.path.join('/app/tmp', f'{time.time_ns()}.mp4')
         else:
-            await message.reply('Неподдерживаемый тип медиа.')
             return
-
+        
         file = await bot.get_file(file_id)
         file_path = file.file_path
-        audio_stream = await bot.download_file(file_path)
-
-        text = await voice_recognizer(audio_stream)
+        if not file_path:
+            return
+        await bot.download_file(file_path, destination=tmp_path)
+            
+        # Передаем тип файла в конвертер
+        text = await voice_recognizer(tmp_path)
         await message.reply(text)
+        os.remove(tmp_path)
     except Exception as e:
-        logger.error(f"Ошибка в media_handler:\n{traceback.format_exc()}")
+        logger.error(f"Ошибка в media_handler:\n{e}\n{traceback.format_exc()}")
         await message.reply('Произошла ошибка при обработке вашего сообщения.')
 
 @dp.message(Command("get_chat_id"))
-async def get_chat_id(message: types.Message):
+async def get_chat_id(message: types.Message) -> None:
     chat_id = message.chat.id
     await message.reply(f"ID этого чата: {chat_id}")
 
-async def voice_recognizer(audio_stream):
-    converted_audio = await convert_audio(audio_stream)
-    if not converted_audio:
+async def voice_recognizer(audio_path: str) -> str:
+    # Передаем тип файла в конвертер
+    converted_audio = await convert_audio(audio_path)
+    if converted_audio is None:
         return "Не удалось преобразовать аудио."
 
     try:
-        # Загрузка аудио данных из объекта BytesIO
         converted_audio.seek(0)
         data, samplerate = sf.read(converted_audio)
-
-        # Передача аудиоданных напрямую в pipe без преобразования в тензор
-        transcription = pipe(data, chunk_length_s=30)["text"]
-        return transcription.strip()
+        transcription = pipe(data)["text"]
+        return str(transcription.strip())
     except Exception:
         logger.error(f"Ошибка при транскрипции:\n{traceback.format_exc()}")
         return "Не удалось транскрибировать аудио."
 
-async def convert_audio(audio_data):
+
+async def convert_audio(audio_path: str) -> io.BytesIO | None:    
+    # Определяем формат входного файла
     ffmpeg_cmd = [
         'ffmpeg',
-        '-i', 'pipe:0',               # Вход из stdin
-        '-vn',                        # Игнорировать видео (если это видеофайл)
-        '-f', 'wav',                  # Формат вывода
-        '-ar', '16000',               # Изменение частоты дискретизации на 16000 Гц
-        '-ac', '1',                   # Убедиться, что канал только один (моно)
-        'pipe:1',                     # Вывод в stdout
-        '-y',                         # Перезапись выходных файлов
-        '-loglevel', 'error'          # Подавление ненужного вывода
+        '-i', audio_path,         # Вход из stdin
+        '-vn',                    # Игнорировать видео
+        '-f', 'wav',              # Формат вывода
+        '-ar', '16000',           # Частота дискретизации
+        '-ac', '1',               # Моно-аудио
+        'pipe:1',                 # Вывод в stdout
+        '-y',                     # Перезапись выходных файлов
+        '-loglevel', 'error'      # Подавление ненужного вывода
     ]
+    
     try:
         process = await asyncio.create_subprocess_exec(
             *ffmpeg_cmd,
@@ -130,17 +136,22 @@ async def convert_audio(audio_data):
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE
         )
-        out, err = await process.communicate(input=audio_data.getvalue())
+
+        # Асинхронная запись и чтение
+        stdout, stderr = await process.communicate()
+
+        logger.info(stderr)
+        
         if process.returncode != 0:
-            logger.error(f"Ошибка FFmpeg: {err.decode()}")
+            logger.error(f"FFmpeg error: {stderr.decode()}")
             return None
-        else:
-            return io.BytesIO(out)
+            
+        return io.BytesIO(stdout)
     except Exception as e:
-        logger.error(f"Ошибка в convert_audio:\n{traceback.format_exc()}")
+        logger.error(f"Ошибка в convert_audio: {traceback.format_exc()}")
         return None
 
-async def main():
+async def main() -> None:
     logger.info('Запуск бота...')
     await dp.start_polling(bot)
     logger.info('Бот остановлен.')
